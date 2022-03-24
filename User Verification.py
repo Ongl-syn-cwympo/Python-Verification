c7b160daec9a478f0cc89b6c2709ac8a06dd03b5
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-24 23:42:16 +0000

    Create User Verification.py

---
+++ User Verification.py
@@ -0,0 +1,30 @@
+# User Verification ~ Asks the user if they accept what they wrote
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+# Variables and declarations 
+Name = list()
+Messages = list()
+
+while input("Say Hello.") == "Hello": 
+  
+  print("Enter your name")
+  Name.append(input()) # Append in Pyhton takes a single argument, which is the item you want to add to the list.
+  
+  print("Write a short message")
+  
+  #Confiramtion comes from the user's input being "Yes"
+  Message = input()
+  if input("Confirm this message by typing in Yes") == "Yes":
+    Messages.append(Message) # Append in Pyhton takes a single argument, which is the item you want to add to the list.
+    print Name[-1] + ":" + Message # -1 means the last element 
+    print("Message confirmed")
+    break # Stops the program
+  
+  else:
+    print("Message error ~ Not confirmed")
+    print("Have another attempt") 
+    # Ouputs an error message if the input is anything other than "Yes"
+    # Allows the user to retry entering the data
+    
