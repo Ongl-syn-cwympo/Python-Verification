d268a22fa706656500e25ca8b012df00a1a6bd5b
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-27 21:38:48 +0100

    Create DoubleEntry.py

---
+++ DoubleEntry.py
@@ -0,0 +1,9 @@
+# User Verification, Double entry ~ Asks the user to type something in and re-enter it to see if they both match
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+# Program works with letters and integers
+v1 = input("Enter a message");
+v2 = input("re enter this message for confimation");
+print(v1 == v2);
